b777a1b6376cc97cd5635ea382c923a05b340c48
thomaszipf <nachricht@thomaszipf.de>
2022-05-08 00:45:37 +0200

    main

--- wordlist-generator.py
+++ wordlist-generator.py
@@ -101,9 +101,12 @@ filename_wordlist = gen_filename(words)
 write_file(filename_config, words)
 print("generating wordlist ...")
 wordlist = gen_wordlist(words)
+print ('variants generated: ', len(wordlist[0]))
+
+print ('appending config filename to wordlist for traceback ...')
 wordlist[0].append(filename_config)
 
-print ('variants generated: ', len(wordlist[0]))
+
 
 # if you want to see the generated wordlist, you can uncomment this one
 # please consider the size of the list before uncommenting
